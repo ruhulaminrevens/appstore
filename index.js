539225bc7b93b2402f887441906b3d8b801030dc
Ruhul Amin <52210203+RuhulAmin567@users.noreply.github.com>
2021-07-09 22:14:58 +0600

    appstore

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * Lekima - Store Delivery Full React Native Application for Wordpress Woocomerce.
+ *
+ * @author rnlab
+ * @package rn_lekima
+ * @format
+ */
+
+import 'react-native-gesture-handler';
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
